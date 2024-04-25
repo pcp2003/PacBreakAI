4e10458bd6e4a2846bb8996f35e5d5738766cbfb
Reis <80530563+JoaoReis153@users.noreply.github.com>
2024-04-25 23:50:44 +0100

    update file of breakout

--- src/breakout/BreakoutGeneticAlgorithm.java
+++ src/breakout/BreakoutGeneticAlgorithm.java
@@ -40,7 +40,7 @@ public class BreakoutGeneticAlgorithm extends GeneticAlgorithm {
         generatePopulation();
 
         if (mode == Commons.BREAKOUT )
-            FileManager.saveParameters(this.MUTATION_CHANCE, this.MUTATION_PERCENTAGE, this.CUTOFF, this.SELECTION_PARENTS_PERCENTAGE, this.k_tournament, this.k_point, this.seed, "randomValuesBreakout.txt");
+            FileManager.saveParameters(this.MUTATION_CHANCE, this.MUTATION_PERCENTAGE, this.CUTOFF, this.SELECTION_PARENTS_PERCENTAGE, this.k_tournament, this.k_point, this.seed, "bestParametersBreakout.txt");
         else throw new IllegalArgumentException("Não é possível utilizar o modo Pacman durante o breakout");
     }
 
