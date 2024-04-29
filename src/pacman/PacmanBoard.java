c1d54ea859a0ff5e18cd71c7b42b979881239385
Reis <80530563+JoaoReis153@users.noreply.github.com>
2024-04-29 19:26:23 +0100

    bug fixed

--- src/pacman/PacmanBoard.java
+++ src/pacman/PacmanBoard.java
@@ -146,8 +146,6 @@ public class PacmanBoard extends JPanel implements ActionListener {
     @Override
     public void addNotify() {
         super.addNotify();
-
-        initGame();
     }
 
     public void makeMove(int move) {
